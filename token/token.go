c37e35a63e64b7b4887e70ada095d3021ce5f317
Sami Cheema <samitcheema@gmail.com>
2020-12-11 17:04:30 -0500

    first commit

---
+++ token/token.go
@@ -0,0 +1,36 @@
+package token
+
+
+type TokenType string   // Allow us to use different values as TokenType
+
+
+type Token struct {
+    Type TokenType
+    Literal string 
+}
+
+const (
+    // Special types
+    ILLEGAL="ILLEGAL"
+    EOF="EOF"
+
+    // Operators
+    ASSIGN="="
+    ADD="+"
+    
+    // Delimiters   
+    COMMA=","
+    SEMICOLON=";"
+
+    LPAREN="("
+    RPAREN="("
+    LBRACE="{"
+    RBRACE="}"
+
+    // Keywords
+    FUNCTION="FUNCTION"
+    LET="LET"
+
+)
+
+

57a07683970415f0bd20bb21e3bdd2dd8d8d861a
usamatcheema <usamatcheema@gmail.com>
2020-12-12 19:46:42 -0500

    Helper function added

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,14 +2,26 @@ package lexer
 
 // Lexer group
 type Lexer struct {
-	input        string
+	Input        string
 	position     int  // pointing to the current position within input being examined
 	readPosition int  // pointing to the next position after the current one(position)
 	ch           byte // the character which is being examined
 }
 
-// New function...
+// New function create and point to lexer
 func New(input string) *Lexer {
-	l := &Lexer{input: input}
+	l := &Lexer{Input: input}
+	l.readChar()
 	return l
+}
+
+// readChar reads character within input
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.Input) {
+		l.ch = 0
+	} else {
+		l.ch = l.Input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
 }

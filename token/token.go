3f5be55f1fa2a5bd25d8d0b212a5c7dcf21d7d3f
usamatcheema <usamatcheema@gmail.com>
2020-12-29 12:55:51 -0500

    function to check identifier

--- token/token.go
+++ token/token.go
@@ -7,7 +7,25 @@ type Token struct {
 	Literal string
 }
 
+// map to differentiate from
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// Function checks whether identifier is user defined or a language keyword
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
 const (
+	// Identifiers and literals
+	IDENT = "IDENT"
+	INT   = "INT" //Integers
+
 	// Special types
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"

fd7e137deddc91ed57d289c0e42d1304682305ac
usamatcheema <usamatcheema@gmail.com>
2021-01-17 12:25:20 -0500

    turning int into token

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -59,6 +59,10 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
 			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}
@@ -72,7 +76,7 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
-// return true or false based on whether the character is an indentifier
+// return true or false based on whether the identifier is a letter
 func isLetter(ch byte) bool {
 	return 'a' <= ch && 'z' >= ch || 'A' <= ch && 'Z' >= ch || ch == '_' || ch == '!' || ch == '?'
 }
@@ -86,6 +90,22 @@ func (l *Lexer) readIdentifier() string {
 	}
 
 	return l.Input[position:l.position]
+}
+
+// return true or false based on whether the identifier is a numerical
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+// read integer and advance lexer position
+func (l *Lexer) readNumber() string {
+	position := l.position
+
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+
 }
 
 // skip over whitespaces, tab, new line and return

be461d1075595a021858528b254e7e771eea37ef
usamatcheema <usamatcheema@gmail.com>
2021-01-20 23:46:28 -0500

    implementing ast

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/samitcheema/Monkey/token"
+
 type Node interface {
 	TokenLiteral()
 }
@@ -12,4 +14,26 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+type Program struct {
+	Statements []Statement
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+
+	return ""
 }
